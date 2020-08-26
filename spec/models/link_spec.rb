4ddeb2c47153b8a6ec80af573d19695025a7d6c8
fahmisan88 <ahmadfahmisan@gmail.com>
2020-08-27 04:40:22 +0800

    redo testing for Link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,39 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
 
-  it "is valid if it has an URL and a slug" do
+  it "is valid if it has the right URL format" do
     link = Link.new(
       url: "https://www.alongurlwebsite.com/articles/how-to-shorten-links",
-      slug: "ABC1234"
+      slug: "ABC123"
     )
     expect(link.valid?).to be(true)
   end
 
-  it "is invalid if it does not have URL" do
+  it "is invalid if it does not have the right URL format" do
     link = Link.new(
-      slug: "ABC1234"
+      url: "alongurlwebsite.com/articles/how-to-shorten-links",
+      slug: "ABC123"
     )
     expect(link.valid?).to be(false)
   end
 
-  it "is invalid if it does not have slug" do
-    link = Link.new(
-      url: "https://www.alongurlwebsite.com/articles/how-to-shorten-links"
-    )
-    expect(link.valid?).to be(false)
-  end
-
-  it "is invalid if the slug has already exist" do
+  it "should always have unique slugs" do
     link_1 = Link.new(
-      url: "https://www.alongurlwebsite.com/articles/how-to-shorten-links",
-      slug: "ABC1234"
+      url: "https://www.alongurlwebsite.com/articles/how-to-shorten-links"
     )
     link_1.save
 
     link_2 = Link.new(
-      url: "https://www.alongurlwebsite.com/articles/how-to-code-in-ruby",
-      slug: "ABC1234"
+      url: "https://www.alongurlwebsite.com/articles/how-to-code-in-ruby"
     )
-    expect(link_2.valid?).to be(false)
+    link_2.save
+    expect(link_2.valid?).to be(true)
   end
 end
